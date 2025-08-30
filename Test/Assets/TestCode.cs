2dc00599ee020f0a459a69be833c8eaa66580fd8
Kr004682zero@icloud.com <Kr004682zero@icloud.com>
2025-08-30 21:58:41 +0900

    Tutorial

---
+++ Test/Assets/TestCode.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Start() => animator = GetComponent<Animator>();
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("IsAttack");
+        }
+    }
+}
